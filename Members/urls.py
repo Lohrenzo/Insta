983412c3b07642c5b5bb357f0db365190c948fc4
lohrenzo <lorenzofblack@gmail.com>
2024-02-13 18:30:28 +0000

    login render

--- Members/urls.py
+++ Members/urls.py
@@ -1,16 +1,37 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView, EditProfilePageView, CreateProfilePageView
+from .views import (
+    UserRegisterView,
+    UserEditView,
+    PasswordsChangeView,
+    ShowProfilePageView,
+    EditProfilePageView,
+    CreateProfilePageView,
+)
+
 # from django.contrib.auth import views as auth_views
-from .import views
+from . import views
+
 urlpatterns = [
-    path('sign-up/', UserRegisterView.as_view(), name="signup"),
-    path('edit-profile/', UserEditView.as_view(), name="edit-profile"),
+    path("login/", views.user_login, name="login"),
+    path("sign-up/", UserRegisterView.as_view(), name="signup"),
+    path("edit-profile/", UserEditView.as_view(), name="edit-profile"),
     # path('password/', auth_views.PasswordChangeView.as_view(template_name = 'Registration/change-password.html')),
-    path('password/', PasswordsChangeView.as_view(template_name = 'Registration/change-password.html')),
-    path('password-success/', views.password_success, name='password-success'),
-    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='user-profile'),
-    path('edit-profile/<int:pk>/page/', EditProfilePageView.as_view(), name='edit-profile-page'),
-    path('create-profile/page/', CreateProfilePageView.as_view(), name='create-profile-page'),
-    path('fav/<int:id>/', views.add_favourite, name='add-favourite'),
-    path('profile/favourites/', views.favourite_list, name='favourite-list'),
+    path(
+        "password/",
+        PasswordsChangeView.as_view(template_name="Registration/change-password.html"),
+    ),
+    path("password-success/", views.password_success, name="password-success"),
+    path("<int:pk>/profile/", ShowProfilePageView.as_view(), name="user-profile"),
+    path(
+        "edit-profile/<int:pk>/page/",
+        EditProfilePageView.as_view(),
+        name="edit-profile-page",
+    ),
+    path(
+        "create-profile/page/",
+        CreateProfilePageView.as_view(),
+        name="create-profile-page",
+    ),
+    path("fav/<int:id>/", views.add_favourite, name="add-favourite"),
+    path("profile/favourites/", views.favourite_list, name="favourite-list"),
 ]
--- Members/views.py
+++ Members/views.py
@@ -4,89 +4,110 @@ from django.views import generic
 from django.views.generic import DetailView, CreateView
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    UserChangeForm,
+    PasswordChangeForm,
+)
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm, ProfilePageForm
 from Home.models import Profile, Post
+
 # from django.http import HttpResponseRedirect
 
+
 def favourite_list(request):
-    new = Post.objects.filter(favourites = request.user)
-    return render(request, 'favourites.html', {'new': new})
-   
+    new = Post.objects.filter(favourites=request.user)
+    return render(request, "favourites.html", {"new": new})
+
 
 def add_favourite(request, id):
     post = get_object_or_404(Post, id=id)
-    if post.favourites.filter(id = request.user.id).exists():
+    if post.favourites.filter(id=request.user.id).exists():
         post.favourites.remove(request.user)
     else:
         post.favourites.add(request.user)
-    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+    return HttpResponseRedirect(request.META["HTTP_REFERER"])
 
 
 class CreateProfilePageView(CreateView):
     model = Profile
     form_class = ProfilePageForm
-    template_name = 'create-profile-page.html'
+    template_name = "create-profile-page.html"
     # fields = '__all__'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+
 class EditProfilePageView(generic.UpdateView):
     model = Profile
-    template_name = 'edit-profile-page.html'
+    template_name = "edit-profile-page.html"
     # success_url = reverse_lazy('user-profile')
-    success_url = reverse_lazy('home')
-    fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'linkedin_url']
+    success_url = reverse_lazy("home")
+    fields = [
+        "bio",
+        "profile_pic",
+        "website_url",
+        "facebook_url",
+        "twitter_url",
+        "instagram_url",
+        "linkedin_url",
+    ]
+
 
 class ShowProfilePageView(DetailView):
-    model = Profile 
-    template_name = 'user-profile.html'
+    model = Profile
+    template_name = "user-profile.html"
 
     def get_context_data(self, *args, **kwargs):
         # users = Profile.objects.all()
         context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
-        
-        page_user = get_object_or_404(Profile, id = self.kwargs['pk'])
+
+        page_user = get_object_or_404(Profile, id=self.kwargs["pk"])
 
         context["page_user"] = page_user
         return context
 
+
 class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangingForm
     # form_class = PasswordChangeForm
-    success_url = reverse_lazy('password-success')
+    success_url = reverse_lazy("password-success")
+
 
 def password_success(request):
-    return render(request, 'password-success.html', {})
+    return render(request, "password-success.html", {})
+
 
 def user_login(request):
-    if request.method=='POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username = username, password =password)
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
             # return redirect('home')
-            return redirect(request.GET.get('next', reverse('home')))
+            return redirect(request.GET.get("next", reverse("home")))
         else:
-            messages.info(request,'Invalid Login Credentials')
-            return redirect('login')
+            messages.info(request, "Invalid Login Credentials")
+            return redirect("login")
     else:
-        return render(request,'login.html')
+        return render(request, "login.html")
+
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
-    template_name = 'register.html'
-    success_url = reverse_lazy('login')
+    template_name = "register.html"
+    success_url = reverse_lazy("login")
+
 
 class UserEditView(generic.UpdateView):
     form_class = EditProfileForm
-    template_name = 'edit_profile.html'
-    success_url = reverse_lazy('home')
+    template_name = "edit_profile.html"
+    success_url = reverse_lazy("home")
 
     def get_object(self):
         return self.request.user
