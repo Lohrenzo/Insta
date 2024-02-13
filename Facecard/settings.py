8f236e5d52dd31e414d35924dc8f8d997eb0c723
lohrenzo <lorenzofblack@gmail.com>
2024-02-13 12:47:36 +0000

    settings

--- Facecard/settings.py
+++ Facecard/settings.py
@@ -23,14 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = django-insecure-(v^+aq7nh166a-g%nrp9s)-=@&v5^+=l(_-%@zq(qd^k7(g-jo
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+# DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = True
 
-
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Login Url added if not @login_required will be giving error
 LOGIN_URL = "/login"
